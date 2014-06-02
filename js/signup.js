c031a8ee76adaf9e6256a044de2e6a2c61301862
Max Rogers <maxrogers90@gmail.com>
2014-06-02 15:17:26 -0400

    got sign up to work

---
+++ js/signup.js
@@ -0,0 +1,20 @@
+$(function() {
+	$("#signup_btn").click(function(){
+		if($("#password_input").val() === $("#password_input2").val()){
+			var json_builder = {};
+			json_builder.email = $("#email_input").val();
+			json_builder.password = $("#password_input").val();
+			$.ajax({
+				url: "http://localhost:9393/new_user",
+				type: "GET",
+				dataType: "jsonp",
+				contentType: 'application/json',
+				data: json_builder,
+				accepts: "application/json",
+				success: function(response){ alert(response);}
+			});
+		}else{
+			alert("passwords don't match");
+		}
+	});
+});
